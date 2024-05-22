73a12b9548fb02c0ca548f9eecfe564095f945d6
Shams Benhamou <shamsbenhamou@gmail.com>
2024-05-22 12:03:37 +0200

    feat: add pdf generator service

--- pdf-webapp/src/Controller/GeneratePdfController.php
+++ pdf-webapp/src/Controller/GeneratePdfController.php
@@ -7,33 +7,18 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 use App\Form\GeneratePdfType;
-use Symfony\Contracts\HttpClient\HttpClientInterface;
-use Symfony\Component\DependencyInjection\ParameterBag\ParameterBagInterface;
+use App\Service\PdfGeneratorService;
 
 class GeneratePdfController extends AbstractController
 {
     #[Route('/generate-pdf', name: 'app_generate_pdf')]
-    public function index(Request $request, HttpClientInterface $client, ParameterBagInterface $parameterBag): Response
+    public function index(Request $request, PdfGeneratorService $pdfGeneratorService): Response
     {
         $form = $this->createForm(GeneratePdfType::class);
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
-            // Appel vers le micro service
-            $hostMicroservice = $parameterBag->get('PDF_URL');
-
-            $response = $client->request('POST', $hostMicroservice . '/converturl', [
-                'body' => [
-                    'url' => $form->get('url')->getData()
-                ]
-            ]);
-
-            // Vérifiez si le statut de la réponse est 200 OK
-            if ($response->getStatusCode() !== 200) {
-                throw new \Exception('Erreur lors de la génération du PDF.');
-            }
-
-            $pdfContent = $response->getContent();
+            $pdfContent = $pdfGeneratorService->generatePdf($form->get('url')->getData());
 
             $pdfResponse = new Response($pdfContent);
             $pdfResponse->headers->set('Content-Type', 'application/pdf');
